b586b317cec19fe0483db47af06271deea82daf4
stephen dewyer <stephen.dewyer@stephengdewyer.info>
2023-10-23 08:43:13 -0500

    updating splide

--- src/lib/components/sliders/Impact.svelte
+++ src/lib/components/sliders/Impact.svelte
@@ -1,56 +1,73 @@
 <script lang="ts">
-    import { Splide, SplideSlide, SplideTrack } from '@splidejs/svelte-splide';
-    import '@splidejs/svelte-splide/css';
-    import SliderImage01 from '$lib/images/slides/slideshow_01/Erin_Maye_Quade_and_stephen_dewyer_13_August_2018.jpg';
-    import SliderImage03 from '$lib/images/slides/slideshow_01/Katie_for_Ward_10_endorsed.jpg';
+  import { Splide, SplideSlide, SplideTrack } from '@splidejs/svelte-splide';
+  import '@splidejs/svelte-splide/css';
+  import SliderImage01 from '$lib/images/slides/slideshow_01/Erin_Maye_Quade_and_stephen_dewyer_13_August_2018.jpg';
+  import SliderImage03 from '$lib/images/slides/slideshow_01/Katie_for_Ward_10_endorsed.jpg';
+  import { onMount } from 'svelte';
 
-    const options = {
-        rewind  : true,
-        gap     : '1rem',
-        autoplay: true,
-        arrows  : true,
+  // set up carousel config
+  const mainOptions = {
+    pagination: true,
+    type: "loop",
+    gap     : '1rem',
+    autoplay: true,
+    arrows  : true,
+  };
+  const thumbsOptions = {
+    arrows: false,
+    gap: 5,
+    isNavigation: true,
+    pagination: false,
+    perMove: 1,
+    perPage: 4,
+    type: "loop",
+    updateOnMove: true,
+  };
+
+  // sync carousels
+  let main: any;
+  let thumbs: any;
+  onMount(() => {
+    if (main && thumbs) {
+      console.log({ main, thumbs });
+      main.sync(thumbs.splide);
     }
+  });
 
 </script>
-<Splide hasTrack={ false } aria-label="..." options={ options } >
-    <SplideTrack>
-      <SplideSlide class="slide">
-        <img src={SliderImage01} alt="public arts commission Founder stephen dewyer handshakes with Erin Maye Quade in front of public art in Northfield, Minnesota on 13 August 2018" />
-      </SplideSlide>
-      <SplideSlide class="slide">
-        <img src={SliderImage03} alt="Katie Jones for Ward 10 endorsed by public arts commission" />
-      </SplideSlide>
-    </SplideTrack>
-  
-    <div class="splide__arrows">
-      <button class="splide__arrow splide__arrow--prev">
-        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 329.46 500">
-            <defs>
-              <style>
-                .cls-12 {
-                  fill: inherit;
-                  stroke-width: 0px;
-                }
-              </style>
-            </defs>
-            <polygon class="cls-12" points="329.46 250 103.48 500 0 500 225.98 250 0 0 103.48 0 329.46 250"/>
-          </svg>
-      </button>
-      <button class="splide__arrow splide__arrow--next">
-        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 329.46 500">
-            <defs>
-              <style>
-                .cls-12 {
-                  fill: inherit;
-                  stroke-width: 0px;
-                }
-              </style>
-            </defs>
-            <polygon class="cls-12" points="329.46 250 103.48 500 0 500 225.98 250 0 0 103.48 0 329.46 250"/>
-          </svg>
-      </button>
+<div>
+  <div class="splide" >
+    <div class="gallery_main">
+      <Splide 
+        bind:this={main} 
+        options={ mainOptions }
+        aria-label="impact splide main gallery"
+      >
+        <SplideSlide >
+          <img src={SliderImage01} alt="public arts commission Founder stephen dewyer handshakes with Erin Maye Quade in front of public art in Northfield, Minnesota on 13 August 2018" />
+        </SplideSlide>
+        <SplideSlide class="splide__slide">
+          <img src={SliderImage03} alt="Katie Jones for Ward 10 endorsed by public arts commission" />
+        </SplideSlide>
+      </Splide>
     </div>
-</Splide>
+    <div class="gallery_thumbs">
+      <Splide
+        id="gallery--thumbs"
+        bind:this={thumbs} 
+        options={ thumbsOptions }
+        aria-label="impact splide thumbnails"
+      >
+        <SplideSlide >
+          <img src={SliderImage01} alt="public arts commission Founder stephen dewyer handshakes with Erin Maye Quade in front of public art in Northfield, Minnesota on 13 August 2018" />
+        </SplideSlide>
+        <SplideSlide class="splide__slide">
+          <img src={SliderImage03} alt="Katie Jones for Ward 10 endorsed by public arts commission" />
+        </SplideSlide>
+      </Splide>
+    </div>
+  </div>
+</div>
 
 <style>
 
