c5b3d5932ec11a49346cfdf38033f0ffdbdcd04e
stephen dewyer <stephen.dewyer@stephengdewyer.info>
2023-10-23 09:35:59 -0500

    updating story

--- src/routes/story/+page.svelte
+++ src/routes/story/+page.svelte
@@ -6,20 +6,45 @@
     import ExhibitingPanel from '$lib/components/howWeWork/Exhibiting.svelte';
     import PlatformingPanel from '$lib/components/howWeWork/Platforming.svelte';
     import ConnectingPanel from '$lib/components/howWeWork/Connecting.svelte';
-    import ImpactSlider from '$lib/components/sliders/Impact.svelte';
     import DownloadButton from '$lib/components/buttons/DownloadButton.svelte';
-  import { onMount } from 'svelte';
+    import { Splide, SplideSlide } from '@splidejs/svelte-splide';
+    import '@splidejs/svelte-splide/css';
+    import SliderImage01 from '$lib/images/slides/slideshow_01/Erin_Maye_Quade_and_stephen_dewyer_13_August_2018.jpg';
+    import SliderImage03 from '$lib/images/slides/slideshow_01/Katie_for_Ward_10_endorsed.jpg';
+    import { afterUpdate } from 'svelte';
+
+    // set up carousel config
+    const mainOptions = {
+        pagination: true,
+        type: "loop",
+        gap     : '1rem',
+        autoplay: true,
+        arrows  : true,
+    };
+    const thumbsOptions = {
+        arrows: false,
+        gap: 5,
+        isNavigation: true,
+        pagination: false,
+        perMove: 1,
+        perPage: 4,
+        type: "loop",
+        updateOnMove: true,
+    };
+
+    // sync carousels
+    let main: any = Splide;
+    let thumbs: any = SplideSlide;
+
+    $: if (main && thumbs) {
+        afterUpdate(() => {
+        // console.log({ main, thumbs });
+        main.sync(thumbs.splide);
+        });
+    };
 
     let activeProcessTab: number = 0;
     $: activeProcessTab;
-
-    $: slider = ImpactSlider;
-
-    let sliderImpact: any;
-
-    onMount(() => {
-        sliderImpact = slider;
-    })
 
     const loginTabPanels: tabPanels[] = [
 		{
@@ -121,8 +146,37 @@
         impact
     </h2>
     <div class="impact_slider">
-        <svelte:component this={sliderImpact} />
-        <!-- <ImpactSlider /> -->
+        <div class="splide" >
+            <div class="gallery_main">
+            <Splide 
+                bind:this={ main } 
+                options={ mainOptions }
+                aria-label="impact splide main gallery"
+            >
+                <SplideSlide >
+                <img src={SliderImage01} alt="public arts commission Founder stephen dewyer handshakes with Erin Maye Quade in front of public art in Northfield, Minnesota on 13 August 2018" />
+                </SplideSlide>
+                <SplideSlide class="splide__slide">
+                <img src={SliderImage03} alt="Katie Jones for Ward 10 endorsed by public arts commission" />
+                </SplideSlide>
+            </Splide>
+            </div>
+            <div class="gallery_thumbs">
+            <Splide
+                id="gallery--thumbs"
+                bind:this={ thumbs } 
+                options={ thumbsOptions }
+                aria-label="impact splide thumbnails"
+            >
+                <SplideSlide >
+                <img src={SliderImage01} alt="public arts commission Founder stephen dewyer handshakes with Erin Maye Quade in front of public art in Northfield, Minnesota on 13 August 2018" />
+                </SplideSlide>
+                <SplideSlide class="splide__slide">
+                <img src={SliderImage03} alt="Katie Jones for Ward 10 endorsed by public arts commission" />
+                </SplideSlide>
+            </Splide>
+            </div>
+        </div>
     </div>
     <h2 class="story_section_heading">
         organization
