0d49c6c2ecd6e06b363e5e1eae0f2bec49eabce7
stephen dewyer <stephen.dewyer@stephengdewyer.info>
2025-07-13 13:33:46 -0700

    updating priorities

--- src/routes/priorities/+page.svelte
+++ src/routes/priorities/+page.svelte
@@ -1042,7 +1042,7 @@
                                     Support family formation and families for humanity
                                 </span>
                                 <div slot="details">
-                                    Art workers largely want, plan and/or have already started to have families.  Supporting families and the formation of families is essential for not only art workers, but humanity as a whole.  Falling rates of family formation/birth rates cannot be adequately addressed and should not be addressed by immigration.  Immigration is only a band-aid for the crisis of declining family formations/birth rates.  We can adequately increase family formation/increased birth rates by:
+                                    Art workers largely want, plan and/or have already started to have families.  Supporting families and the formation of families is essential for not only art workers, but humanity as a whole.  Falling rates of family formation/birth rates cannot be adequately addressed and should not be addressed by immigration.  Immigration is only a band-aid for the crisis of declining family formations/birth rates.  Economic reasons are the primary cause for abortions.  We can adequately increase family formation/increased birth rates by:
                                     <ol>
                                         <li>
                                             Increase jobs that support family formation.  Jobs that support family formation are ones that pay enough to raise a family, have stability and opportunities for growth.
